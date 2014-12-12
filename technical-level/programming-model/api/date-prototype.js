b40edf83ade0a853f945d9e19289a2cf8f7c1d26
Plamen Todorov <pl.todorov@gmail.com>
2014-12-12 23:19:58 +0200

    date prototype

---
+++ technical-level/programming-model/api/date-prototype.js
@@ -0,0 +1,10 @@
+var debug = require('debug')('object-prototoype');
+var test = require('tape');
+
+test('### getTime and valueOf ###', function(t) {
+
+  var birthday = new Date(2000, 2, 2);
+  t.equal(birthday.getTime(), birthday.valueOf(), "The value returned by the getTime() and valueOf() methods is the number of milliseconds since 1 January 1970 00:00:00 UTC")
+    
+  t.end();
+});
