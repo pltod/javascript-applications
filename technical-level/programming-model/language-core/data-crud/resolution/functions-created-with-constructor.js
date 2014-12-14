a8fed1a7aa0b179d70ab2835eb12e86566302c4e
Plamen Todorov <pl.todorov@gmail.com>
2014-12-14 20:08:09 +0200

    more clear explanation

--- technical-level/programming-model/language-core/data-crud/resolution/functions-created-with-constructor.js
+++ technical-level/programming-model/language-core/data-crud/resolution/functions-created-with-constructor.js
@@ -1,7 +1,7 @@
 var debug = require('debug')('functions-created-with-constructor');
 var test = require('tape');
 
-test('### functions created with new ###', function(t) {
+test('### functions created with new does not store lexical scope ###', function(t) {
   var x = 20;
 
   var f = function() {
@@ -9,8 +9,8 @@ test('### functions created with new ###', function(t) {
   };
   var bar = new Function('return x');
 
-  t.equal(f(), 20, "the scope of functions created with with literal or expression is lexical");
-  t.throws(bar, ReferenceError, "the scope of functions created with new is global scope");
+  t.equal(f(), 20, "the scope of functions created with literal or expression is lexical");
+  t.throws(bar, ReferenceError, "the scope of functions created with new is global scope - they does not store lexical scope");
 
   t.end();
 });
