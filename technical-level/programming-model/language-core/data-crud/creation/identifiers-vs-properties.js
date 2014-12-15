e1034eb8528b873fa4cc275777e5503c009064de
Plamen Todorov <pl.todorov@gmail.com>
2014-12-15 16:33:05 +0200

    identifiers vs properties

---
+++ technical-level/programming-model/language-core/data-crud/creation/identifiers-vs-properties.js
@@ -0,0 +1,5 @@
+// Here is the place to explain the difference
+// Both are means to access data of different types
+
+// In case of properties when point to function are usually accessor properties
+// Properties can be internal as well -> For example, [[Prototype]] holds the prototype of an object and is readable via Object.getPrototypeOf().
