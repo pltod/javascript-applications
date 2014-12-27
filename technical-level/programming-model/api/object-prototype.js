a3ece9f29858ab2fd3cbc94619c36603c4d186bb
Plamen Todorov <pl.todorov@gmail.com>
2014-12-27 15:11:59 +0200

    Object.create more tests

--- technical-level/programming-model/api/object-prototype.js
+++ technical-level/programming-model/api/object-prototype.js
@@ -2,6 +2,17 @@ var debug = require('debug')('object-prototoype');
 var test = require('tape');
 
 test('### Object.create() ###', function(t) {
+  t.throws(Object.create, TypeError, 'Object.create could only be invoked with null or obj that plays prototype role');
+  t.end();
+});
+
+test('### Object.create(null) ###', function(t) {
+  var v = Object.create(null);
+  t.notOk(Object.getPrototypeOf(v), "the created object has not prototype");
+  t.end();
+});
+
+test('### Object.create(prototype) ###', function(t) {
   var obj = {
     prop: "test"
   }
