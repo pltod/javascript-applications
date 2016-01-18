de59e331a884fa0783ed916aabe13d98bfe35f14
Plamen Todorov <pl.todorov@gmail.com>
2016-01-18 06:09:02 +0200

    shorthands / capturing groups / look ahead / whole words

--- technical-level/programming-model/api/regular-expressions.js
+++ technical-level/programming-model/api/regular-expressions.js
@@ -1,4 +1,5 @@
-//Test suite is based on https://egghead.io/lessons/javascript-javascript-regular-expressions-introduction
+// Test suite is based on https://egghead.io/lessons/javascript-javascript-regular-expressions-introduction
+// The source code is at https://github.com/joemaddalone/egghead-regular-expressions
 
 var debug = require('debug')('regular-expressions');
 var test = require('tape');
@@ -135,5 +136,96 @@ test('### Using quantifiers - ? = {0,1} ###', function(t) {
   var regexp1 = /b?/g;
   t.equal(1, regexp.exec(str)[0].length, 'returns string with length 1 when found');
   t.equal("", regexp1.exec(str)[0], 'returns empty string when missing');
+  t.end();
+});
+
+test('### Using quantifiers - greedy quantifier ###', function(t) {
+  var str = "one two three";
+  var regexp = /.+/g;
+  t.deepEqual(["one two three"], str.match(regexp), 'greedy quantifier');
+  t.end();
+});
+
+test('### Character class and ranges ###', function(t) {
+  var str = "cat math fat ?at Math";
+  var regexp = /[cm]at/g;
+  var regexp5 = /[mc]at/g;
+  var regexp1 = /[^cm]at/g;
+  var regexp2 = /[a-z]at/g;
+  var regexp3 = /[a-zA-Z]at/g; //digit range 0-9
+  var regexp4 = /[?]at/g;
+  t.deepEqual(["cat", "mat"], str.match(regexp), 'character class specifies symbols that can be combined with the pattern');
+  t.deepEqual(["cat", "mat"], str.match(regexp5), 'the order of the symbols in character class is not important');
+  t.deepEqual(["fat", "?at", "Mat"], str.match(regexp1), 'negated charcter class with ^');
+  t.deepEqual(["cat", "mat", "fat"], str.match(regexp2), 'we can specify ranges');
+  t.deepEqual(["cat", "mat", "fat", "Mat"], str.match(regexp3), 'and union of ranges');
+  t.deepEqual(["?at"], str.match(regexp4), 'special symbols not excaped in character class');
+  t.end();
+});
+
+test('### Shorthands for common sets ###', function(t) {
+  var str = "one 1";
+  var regexp = /\w/g;
+  var regexp1 = /\d/g;
+  var regexp2 = /\s/g;
+  var regexp3 = /\S/g;
+  var regexp4 = /[^\s]/g;
+  t.deepEqual(["o", "n", "e", "1"], str.match(regexp), 'w is for alfanumeric');
+  t.deepEqual(["1"], str.match(regexp1), 'd is for digits');
+  t.deepEqual([" "], str.match(regexp2), 's is for whitespace');
+  t.deepEqual(["o", "n", "e", "1"], str.match(regexp3), 'negation with capital letter');
+  t.deepEqual(["o", "n", "e", "1"], str.match(regexp4), 'or negation with characrter class starting with caret');
+  t.end();
+});
+
+test('### Capturing groups to find groups of characters ###', function(t) {
+  var str = "foobar foobuz foo footest";
+  var regexp = /foo(bar)/g;
+  var regexp1 = /foo(bar|buz)/g;
+  var regexp2 = /foo(bar|buz)?/g;
+  t.deepEqual(["foobar"], str.match(regexp), 'capturing group to find group of characters');
+  t.deepEqual(["foobar", "foobuz"], str.match(regexp1), 'with | symbol several groups could be specified');
+  t.deepEqual(["foobar", "foobuz", "foo", "foo"], str.match(regexp2), 'quontifiers together with capturing group');
+  t.end();
+});
+
+test('### Capturing groups and references useful for string replacement ###', function(t) {
+  var str = "test foobar";
+  var regexp = /foo(bar)/g;
+  var regexp1 = /foo(?:bar)/g;
+  t.equal("test bar", str.replace(regexp, '$1'), 'the match is replaced by the capturing group item value referenced with $1');
+  t.equal("test $1", str.replace(regexp1, '$1'), 'opt out for creating $ references in capturing group with ?: symbol');
+  t.end();
+});
+
+test('### Look Ahead ###', function(t) {
+  var str = "foobuz foobar";
+  var regexp = /foo(?=bar)/g;
+  var regexp1 = /foo(?!bar)/g;
+  t.deepEqual(["foo"], str.match(regexp), 'Look ahead for matching what has to be after the pattern');
+  t.deepEqual(["foo"], str.match(regexp1), 'Look ahead for matching what has not to be after the pattern');
+  t.end();
+});
+
+test('### Matching whole words ###', function(t) {
+  var str = "This history is his";
+  var regexp = /\bis\b/g; //start/ends with is
+  var regexp1 = /is\b/g; //end with is
+  var regexp2 = /\Bis/g; //is not the beginning
+  t.deepEqual(["is"], str.match(regexp), 'match the whole word');
+  t.deepEqual(["is", "is", "is"], str.match(regexp1), 'match the word that begins with something');
+  t.deepEqual(["is", "is", "is"], str.match(regexp2), 'match words that does not begin with something');
+  t.end();
+});
+
+test('### Back References ###', function(t) {
+  //TODO
+  //
+  t.end();
+});
+
+test('### Line Anchor ###', function(t) {
+  //TODO
+  // ^ and $ for marking the begin and end of a line
   t.end();
 });
